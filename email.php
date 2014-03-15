927c8e3f7b5e0637ab1b7d2c8057c9b0efdb184f
Ryan Franklin <investigence@gmail.com>
2014-03-15 12:42:52 -0400

    testing double_optin=true/false parameter in subscribe.php

--- email.php
+++ email.php
@@ -1,13 +1,37 @@
 <?php
-include('SMTPconfig.php');
-include('SMTPClass.php');
-//if($_SERVER["REQUEST_METHOD"] == "POST")
-//{
-$to = 'jody@introapp.net';
-$from = 'jody@introapp.net';
-$subject = 'what a country';
-$body = 'my country is of thee, sweet land of liberty, of thee I sing';
-$SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
-$SMTPChat = $SMTPMail->SendMail();
-//}
+require 'Mandrill.php';
+
+$mandrill = new Mandrill();
+
+$message = array(
+    'subject' => 'Test message',
+    'from_email' => 'jody@introapp.net',
+    'html' => '<p>this is a test message with Mandrill\'s PHP wrapper!.</p>',
+    'to' => array(array('email' => 'jody@introapp.net', 'name' => 'Jody')),
+    'merge_vars' => array(array(
+        'rcpt' => 'jody@introapp.net',
+        'vars' =>
+        array(
+            array(
+                'name' => 'FIRSTNAME',
+                'content' => 'Jody'),
+            array(
+                'name' => 'LASTNAME',
+                'content' => 'Ford')
+    ))));
+
+$template_name = 'Stationary';
+
+$template_content = array(
+    array(
+        'name' => 'main',
+        'content' => 'Hi *|FIRSTNAME|* *|LASTNAME|*, thanks for signing up.'),
+    array(
+        'name' => 'footer',
+        'content' => 'Copyright 2012.')
+
+);
+
+print_r($mandrill->messages->sendTemplate($template_name, $template_content, $message));
+
 ?>
