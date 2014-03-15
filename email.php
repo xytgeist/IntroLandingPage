7b3f27a4c70004a9ec0b0ea30021c0fbacc797ee
Ryan Franklin <investigence@gmail.com>
2014-03-15 12:25:07 -0400

    testing double_optin=true/false parameter in subscribe.php

--- email.php
+++ email.php
@@ -4,7 +4,7 @@ include('SMTPClass.php');
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 $to = 'jody@introapp.net';
-$from = 'ryan@introapp.net';
+$from = 'jody@introapp.net';
 $subject = 'what a country';
 $body = 'my country is of thee, sweet land of liberty, of thee I sing';
 $SMTPMail = new SMTPClient ($SmtpServer, $SmtpPort, $SmtpUser, $SmtpPass, $from, $to, $subject, $body);
